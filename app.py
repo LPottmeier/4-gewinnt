680c912fb14e5e8a92fc32503577529cc7c3b5fb
lucullusTheOnly <email@example.com>
2020-01-13 16:17:56 +0100

    infinite loop

--- app.py
+++ app.py
@@ -10,11 +10,14 @@ player_number = 1
 
 def play():
     board_matrix, current_player = get_status()
-    while(board_matrix):
-        if current_player == player_number:
-            new_turn_position = calculate_drop(get_board(), player_number)
-            make_turn(new_turn_position)
+    while(True):
+        while(board_matrix):
+            if current_player == player_number:
+                new_turn_position = calculate_drop(board_matrix, player_number)
+                make_turn(new_turn_position)
+            board_matrix, current_player = get_status()
         board_matrix, current_player = get_status()
+
 
 def make_turn(row_number):
     r = requests.post("http://connect-4-api-dev1-connect4.apps.cluster-sva-7909.sva-7909.example.opentlc.com/turn",
