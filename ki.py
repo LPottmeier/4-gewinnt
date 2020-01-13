9e03b2d7c1b011e73ad115e6e1e00ddaa59d944c
Ludger Pottmeier <ludger.pottmeier@atos.net>
2020-01-13 14:31:29 +0100

    draft

--- ki.py
+++ ki.py
@@ -13,7 +13,7 @@ def find_drops ( board):
 #
 # 5 moegliche dimensionen auf leerfelder prüfen
 # 
-def rate_drop (board,  zeile, spalte )
+def rate_drop_by_empty (board,  zeile, spalte )
    rating = 0
    # oben
    if zeile == 0:
@@ -38,6 +38,49 @@ def rate_drop (board,  zeile, spalte )
   
    return rating 
 
+#
+# 5 moegliche dimensionen auf neighbour prüfen
+# 
+def rate_drop_by_neighbour (board,  zeile, spalte , myid )
+   rating = 0
+   otherrating = 0
+   # oben
+   if zeile == 0:
+       oben = True
+   # links   
+   if spalte == 0:
+        links = True
+   # rechts   
+   if spalte == 7:
+        rechts = True
+   for case in range(0,4):
+       if case == 0 and not links 
+            if board [zeile, spalte-1] == myid :
+                rating = rating +1
+            else if board [zeile, spalte-1] != 0 :
+                otherrating = otherrating +1
+       if case == 1 and  not links and not oben 
+            if board [zeile -1, spalte-1] == myid :
+                    rating = rating +1;
+            else if board [zeile -1, spalte-1] != 0 :
+              otherrating = otherrating +1
+       if case == 2 and  not oben and 
+            if board [zeile -1, spalte] == myid :
+                rating = rating +1;
+            else if board [zeile -1, spalte] != 0 :
+                      otherrating = otherrating +1
+       if case == 3 and not rechts and not oben and 
+            if board [zeile -1 , spalte +1] == myid :
+                rating = rating +1;
+            else if board [zeile -1 , spalte +1] != 0 :
+                      otherrating = otherrating +1
+        if case == 4 and not rechts and 
+            if board [zeile + 1 , spalte + 1 ] == myid :
+                rating = rating +1;
+            else if board [zeile + 1 , spalte + 1 ] != 0 :
+                       otherrating = otherrating +1
+   return rating + otherrating  
+
 def keywithmaxval(d):
      """ a) create a list of the dict's keys and values; 
          b) return the key with the max value"""  
@@ -45,12 +88,12 @@ def keywithmaxval(d):
      k=list(d.keys())
      return k[v.index(max(v))]
 
- def calculate_drop ( board , myid):
+def calculate_drop ( board , myid):
     possible_drops = find_drops( board)
     myrates = {}
     # calculate rates
     for key, element in possible_drops.items():
-         myrates[key] = rate_drop (board , key, element, myid)
+         myrates[key] = rate_drop_by_empty (board , key, element) +  rate_drop_by_neighbour(board , key, element, myid)
     #  find best for us
 
     return keywithmaxval(myrates)
